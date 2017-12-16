778319bff132f298b10420be9ef8f048780cf624
Prabhat Kumar <pkprabhat@users.noreply.github.com>
2017-12-16 23:26:19 +0530

    Update with outputs and accuracy parameters

--- myMLproject.pm
+++ myMLproject.pm
@@ -16,6 +16,7 @@ testing <- read.csv("pml-testing.csv", na.strings = c("NA", ""))
 training <- training[, colSums(is.na(training)) == 0]
 testing <- testing[, colSums(is.na(testing)) == 0]
 
+## Ignore some data which might not be useful
 trainData <- training[, -c(1:7)]
 testData <- testing[, -c(1:7)]
 
@@ -29,3 +30,39 @@ print(fit_rf)
 
 predict_rf <- predict(fit_rf, valid)
 predict_final  <- predict(fit_rf, testData)
+conf_rf <- confusionMatrix(valid$classe, predict_rf)
+accuracy_rf <- conf_rf$overall[1]
+
+
+####Sample Run and Final Output##########
+#Random Forest 
+
+#13737 samples
+#52 predictor
+#5 classes: 'A', 'B', 'C', 'D', 'E' 
+
+#No pre-processing
+#Resampling: Bootstrapped (25 reps) 
+#Summary of sample sizes: 13737, 13737, 13737, 13737, 13737, 13737, ... 
+#Resampling results across tuning parameters:
+  
+#  mtry  Accuracy   Kappa    
+#2    0.9887293  0.9857383
+#27    0.9887483  0.9857637
+#52    0.9801501  0.9748835
+
+#Accuracy was used to select the optimal model using the largest value.
+#The final value used for the model was mtry = 27.
+
+#> predict_rf <- predict(fit_rf, valid)
+#> predict_final  <- predict(fit_rf, testData)
+
+#> conf_rf <- confusionMatrix(valid$classe, predict_rf)
+#> accuracy_rf <- conf_rf$overall[1]
+#> accuracy_rf
+#Accuracy 
+#0.9935429 
+
+#> predict_final
+#[1] B A B A A E D B A A B C B A E E A B B B
+#Levels: A B C D E
